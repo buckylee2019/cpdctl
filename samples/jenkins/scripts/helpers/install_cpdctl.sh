8f71a8c06bc0b44bddf4a0fcafd79a66e0f32146
buckylee <bucky.yh.lee@ibm.com>
2022-10-25 16:06:04 +0800

    modify jq

--- samples/jenkins/scripts/helpers/install_cpdctl.sh
+++ samples/jenkins/scripts/helpers/install_cpdctl.sh
@@ -1,4 +1,4 @@
-
+sudo apt install -y jq
 curl -s https://api.github.com/repos/IBM/cpdctl/releases | \
   jq -r '.[0].assets[] | select (.name == "cpdctl_linux_amd64.tar.gz") | .url'  | \
   xargs -I {} curl -sSL -H 'Accept: application/octet-stream' "{}" -o cpdctl_linux_amd64.tar.gz
