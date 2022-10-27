53b102f7d088686aebf650b17b09a9514a6df43b
buckylee <bucky.yh.lee@ibm.com>
2022-10-27 15:42:21 +0800

    update software spec

--- samples/jenkins/scripts/dev/promote_to_dev_space.sh
+++ samples/jenkins/scripts/dev/promote_to_dev_space.sh
@@ -10,6 +10,7 @@ export PATH=$PATH:$PWD
 env_name=jupconda38
 job_name=train-scikit-model-job
 trained_model_id_file=./trained_model_id
+software_specification_name='runtime-22.1-py3.9'
 
 export CPD_SCOPE=cpd://cpd402-demo/projects/$PROJECT_ID
 
@@ -30,6 +31,26 @@ promote_asset () {
 trained_model_id=$(<${trained_model_id_file})
 regression_data_asset_id=$(find_asset data_asset "credit_risk_regression.csv")
 evaluation_script_id=$(find_asset script "evaluate_model*")
+
+software_id=$(cpdctl environment software-specification list --space-id "$DEV_SPACE_ID" --name "$software_specification_name" --output json --jmes-query 'resources[0].metadata.asset_id' --raw-output)
+
+cat > softwarespec.json <<-EOJSON
+[
+  {
+    "op": "add",
+    "path": "/software_spec",
+    "value": {
+    "base_id": "$software_id",
+    "name": "$software_specification_name"
+  }
+}
+]
+EOJSON
+
+cpdctl asset attribute update --space-id "$DEV_SPACE_ID" --asset-id "$evaluation_script_id" --attribute-key script  --json-patch '@./softwarespec.json'
+
+
+
 
 promote_asset "trained model" $trained_model_id
 promote_asset "regression data asset" $regression_data_asset_id
