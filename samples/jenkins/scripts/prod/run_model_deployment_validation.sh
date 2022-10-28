1630943ead2c4b6056ab0567a0d4f680a4486990
buckylee <bucky.yh.lee@ibm.com>
2022-10-28 13:39:57 +0800

    #14

--- samples/jenkins/scripts/prod/run_model_deployment_validation.sh
+++ samples/jenkins/scripts/prod/run_model_deployment_validation.sh
@@ -21,9 +21,9 @@ prod_model_id=$(cpdctl asset search --query '*:*' --type-name wml_model --output
   --jmes-query "results[0].metadata.asset_id" --raw-output)
 evaluate_model_job_id=$(find_asset job "evaluate_model_job")
 
-script_batch_deployment_id=$(cpdctl ml deployment list --space-id "$PROD_SPACE_ID" --asset-id "$prod_model_id"  --output json -j 'metadata.id' --raw-output)
+script_batch_deployment_id=$(cpdctl ml deployment list --space-id "$PROD_SPACE_ID" --asset-id "$prod_model_id"  --output json --jmes-query 'resources[0].metadata.id' --raw-output)
 cpdctl ml deployment list --space-id "$PROD_SPACE_ID"
-#echo "Script Batch Deploy id: $script_batch_deployment_id"
+echo "Script Batch Deploy id: $script_batch_deployment_id"
 cat > scoring.json <<-EOJSON
  {
     "input_data_references": [
