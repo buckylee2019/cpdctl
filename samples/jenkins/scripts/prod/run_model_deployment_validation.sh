0251b34f8ca977b82fc4cdba588d129ac3393e62
buckylee <bucky.yh.lee@ibm.com>
2022-10-28 13:54:31 +0800

    #15

--- samples/jenkins/scripts/prod/run_model_deployment_validation.sh
+++ samples/jenkins/scripts/prod/run_model_deployment_validation.sh
@@ -7,6 +7,8 @@ export PATH=$PATH:$PWD
 
 export CPD_SCOPE=cpd://cpd402-demo/spaces/$PROD_SPACE_ID
 export_file=./evaluation_result.zip
+evaluation_output_name=model_evaluation_result.zip
+job_name='evaluate_model_batch_deployment_job'
 
 find_asset () {
   echo "Searching for $1 with name: $2..." >&2
@@ -16,13 +18,17 @@ find_asset () {
   echo $asset_id
 }
 
+
+imported_regression_data_asset_id=$(find_asset data_asset "credit_risk_regression.csv" $PROD_SPACE_ID)
 prod_evaluation_script_id=$(find_asset script "evaluate_model*")
 prod_model_id=$(cpdctl asset search --query '*:*' --type-name wml_model --output json \
   --jmes-query "results[0].metadata.asset_id" --raw-output)
 evaluate_model_job_id=$(find_asset job "evaluate_model_job")
 
+if [ "$evaluate_model_job_id" == "null"]
+then
 script_batch_deployment_id=$(cpdctl ml deployment list --space-id "$PROD_SPACE_ID" --asset-id "$prod_model_id"  --output json --jmes-query 'resources[0].metadata.id' --raw-output)
-cpdctl ml deployment list --space-id "$PROD_SPACE_ID"
+
 echo "Script Batch Deploy id: $script_batch_deployment_id"
 cat > scoring.json <<-EOJSON
  {
@@ -32,7 +38,7 @@ cat > scoring.json <<-EOJSON
         "id": "input",
         "connection": {},
         "location": {
-          "href": "/v2/assets/$imported_regression_data_asset_id?space_id=$test_space_id"
+          "href": "/v2/assets/$imported_regression_data_asset_id?space_id=$PROD_SPACE_ID"
         }
       }
     ],
@@ -47,14 +53,23 @@ cat > scoring.json <<-EOJSON
 }
 EOJSON
 
-# echo "Starting job $job_name..."
+echo "Starting job $job_name..."
 
-# deployment_script_job_id=$(cpdctl ml deployment-job create wait --space-id "$test_space_id" --name "$job_name" \
-#   --deployment '{"id": "'$script_batch_deployment_id'"}' --scoring '@./scoring.json' --output json -j "metadata.id" \
-#   --raw-output)
+deployment_script_job_id=$(cpdctl ml deployment-job create wait --space-id "$PROD_SPACE_ID" --name "$job_name" \
+  --deployment '{"id": "'$script_batch_deployment_id'"}' --scoring '@./scoring.json' --output json -j "metadata.id" \
+  --raw-output)
 
+while [[ "$job_id" == "" || "$job_id" == "null" ]]; do
+  deployment_job=$(cpdctl ml deployment-job get --space-id "$PROD_SPACE_ID" --job-id "$deployment_script_job_id" \
+    --output json)
 
+  evaluate_model_job_id=$(echo $deployment_job | jq '.entity.platform_job.job_id' -r)
+  run_id=$(echo $deployment_job | jq '.entity.platform_job.run_id' -r)
 
+  sleep 1
+done
+
+fi
 software_specification_name='runtime-22.1-py3.9'
 software_id=$(cpdctl environment software-specification list --space-id "$PROD_SPACE_ID" --name "$software_specification_name" --output json --jmes-query 'resources[0].metadata.asset_id' --raw-output)
 
